a1ea85fb16142a4ef80c86a482ab0c692c029429
Roberto Camacho <camachof.roberto@gmail.com>
2019-10-05 17:50:07 -0300

    fix(myaccount): add name

---
+++ models/Match.js
@@ -0,0 +1,22 @@
+// const mongoose = require('mongoose');
+// const Schema = mongoose.Schema;
+
+// const matchSchema = new Schema(
+//   {
+//     petEvaluating: { type: String, required: true },
+//     petEvaluated: { type: String, required: true },
+//     evaluation: {
+//       type: String,
+//       required: true,
+//       enum: ['undecided', 'accepted', 'rejected']
+//     },
+//     date: { Date, required: true }
+//   },
+//   {
+//     timestamps: true
+//   }
+// );
+
+// const Match = mongoose.model('Match', matchSchema);
+
+// module.exports = Match;
--- services/pets/myAccount.js
+++ services/pets/myAccount.js
@@ -1,8 +1,9 @@
 const Pet = require('./../../models/Pet');
 const myAccountGet = async (req, res, next) => {
   const ownerId = req.session.currentUser._id;
+  const name = req.session.currentUser.name;
   const pets = await Pet.find({ owner: { $eq: ownerId } });
-  res.render('myAccount', { pets, ownerId });
+  res.render('myAccount', { pets, ownerId, name });
 };
 
 module.exports = myAccountGet;
